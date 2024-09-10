1564d2baa11793d9406cfcc746c986b9555e69ea
sabita <sabitakhadka091@gmail.com>
2024-09-10 10:28:36 +0545

    feat : Added global ccs of mantine

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { MantineProvider } from "@mantine/core";
 import React from "react";
 import Homepage from "./components/Homepage";
+import "@mantine/core/styles.css";
 
 const App: React.FC = () => {
   return (
