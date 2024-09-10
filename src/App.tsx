4e648d3b80e9c318cc60519c5ffe742eea9dd5f7
sabita <sabitakhadka091@gmail.com>
2024-09-10 09:36:14 +0545

    created new component navbar and homepage and import in app

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import { MantineProvider } from "@mantine/core";
 import React from "react";
+import Homepage from "./components/Homepage";
 
 const App: React.FC = () => {
   return (
     <MantineProvider>
-      <App />
+      <Homepage />
     </MantineProvider>
   );
 };
